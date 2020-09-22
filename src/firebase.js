3a7a70c06c46e742222483e9ba56f4b324fcc248
Juan Benitez <juanbenitez2908@gmail.com>
2020-09-21 19:33:45 -0500

    Codepen clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbiXa3FAB2TfO80DU9HS_w_9M-xGo4Qvg",
+    authDomain: "codepen-clone-3c439.firebaseapp.com",
+    databaseURL: "https://codepen-clone-3c439.firebaseio.com",
+    projectId: "codepen-clone-3c439",
+    storageBucket: "codepen-clone-3c439.appspot.com",
+    messagingSenderId: "627199795625",
+    appId: "1:627199795625:web:f94031fe340827dda5e4b8",
+    measurementId: "G-YHYB5VVX7B"
+  };
